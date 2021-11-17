a9f63c10c8cef5a4d3c50544a8368b98d60d02fa
Kris Algra <kristian.algra@broadcom.com>
2021-11-17 07:42:12 -0700

    add basic message check and rename vars

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require("http");
 const { URL } = require("url");
 
-// let previousReq;
+let previousReq;
 
 // Create an HTTP proxy
 const proxy = http.createServer((clientReq, clientRes) => {
+  previousReq && (previousReq = clientReq);
+
   let chunks = [];
   // previousReq ? setnew : forward request without delay (possible race condition Assume only serial requests look in handling concurency issues)
   // if consecutive requests wait 2 seconds. use setTimeout. Design decision, do I wait to make proxy request or make proxy request then wait to respond back to client.
@@ -14,17 +16,23 @@ const proxy = http.createServer((clientReq, clientRes) => {
     const { url, headers, method } = clientReq;
     const { hostname: host, port, pathname: path } = new URL(url);
     const clientReqBody = Buffer.concat(chunks).toString("utf-8");
+
     // look for bad_message if present reject 401
-    const proxyRequest = http.request(
+    if (clientReqBody.includes("bad_message")) {
+      clientRes.writeHead("401", headers);
+      clientRes.end();
+    }
+
+    const proxyReq = http.request(
       { host, port: port || 80, path, headers, method },
-      (proxyResponse) => {
-        const { statusMessage, statusCode, headers } = proxyResponse;
+      (proxyRes) => {
+        const { statusMessage, statusCode, headers } = proxyRes;
         clientRes.writeHead(statusCode, statusMessage, headers);
-        proxyResponse.on("data", (chunk) => clientRes.write(chunk));
-        proxyResponse.on("end", () => clientRes.end());
+        proxyRes.on("data", (chunk) => clientRes.write(chunk));
+        proxyRes.on("end", () => clientRes.end());
       }
     );
-    proxyRequest.end(clientReqBody);
+    proxyReq.end(clientReqBody);
   });
 });
 
