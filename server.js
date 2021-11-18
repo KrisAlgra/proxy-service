9d5265ce6e2a80238ca7f10da74fb07fbe9209ce
Kris Algra <kristian.algra@broadcom.com>
2021-11-17 19:11:13 -0700

    refactor delay

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const { url } = require("inspector");
 const { URL } = require("url");
 
-let previousReq; //`${clientReq.socket.remoteAddress}-${clientReq.url}-${clientReq.method}`
+let previousReq;
 
 // Create an HTTP proxy
 const proxy = http.createServer((clientReq, clientRes) => {
@@ -18,33 +18,6 @@ const proxy = http.createServer((clientReq, clientRes) => {
   }
   previousReq = `${clientReq.socket.remoteAddress}-${clientReq.url}-${clientReq.method}`;
 
-  //   const isConsecutive = () => {
-  //     if (previousReq) {
-  //       const {
-  //         method: prevMethod,
-  //         url: prevUrl,
-  //         socket: prevSocket,
-  //       } = previousReq;
-  //       const { method: curMethod, url: curUrl, socket: curSocket } = clientReq;
-  //       console.log(
-  //         "requests match: ",
-  //         curMethod === prevMethod &&
-  //           curUrl === prevUrl &&
-  //           curSocket.remoteAddress === prevSocket.remoteAddress
-  //       );
-  //       if (
-  //         curMethod === prevMethod &&
-  //         curUrl === prevUrl &&
-  //         curSocket.remoteAddress === prevSocket.remoteAddress
-  //       ) {
-  //         // previousReq = clientReq;
-  //         return true;
-  //       }
-  //     }
-  //     // previousReq = clientReq;
-  //     return false;
-  //   };
-
   clientReq.on("data", (chunk) => chunks.push(chunk));
   clientReq.on("end", () => {
     const { url, headers, method } = clientReq;
@@ -55,23 +28,21 @@ const proxy = http.createServer((clientReq, clientRes) => {
     if (clientReqBody.includes("bad_message")) {
       clientRes.writeHead("401", headers);
       clientRes.end();
+      return;
     }
 
     const proxyReq = http.request(
       { host, port: port || 80, path, headers, method },
       (proxyRes) => {
         const { statusMessage, statusCode, headers } = proxyRes;
-        if (isConsecutive) {
-          setTimeout(() => {
+        setTimeout(
+          () => {
             clientRes.writeHead(statusCode, statusMessage, headers);
             proxyRes.on("data", (chunk) => clientRes.write(chunk));
             proxyRes.on("end", () => clientRes.end());
-          }, 5000);
-        } else {
-          clientRes.writeHead(statusCode, statusMessage, headers);
-          proxyRes.on("data", (chunk) => clientRes.write(chunk));
-          proxyRes.on("end", () => clientRes.end());
-        }
+          },
+          isConsecutive ? 5000 : 0
+        );
       }
     );
     proxyReq.end(clientReqBody);
